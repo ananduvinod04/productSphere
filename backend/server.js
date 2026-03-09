bd3f94a140b9176592186f82dc4976bf890fabfb
anandu <ananduvinod04@gmail.com>
2026-03-09 12:41:26 +0530

    bug/cors 2

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ app.use(cookieParser())
 
 
 app.use(cors({
-  origin:process.env.CLIENT_URL,
+  origin:process.env.CLIENT_URL || "https://product-sphere-ten.vercel.app/",
+
   credentials: true,
  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
