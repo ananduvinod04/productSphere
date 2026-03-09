54d0d093755a3a3253ee42e7f63384a332e03f40
anandu <ananduvinod04@gmail.com>
2026-03-09 12:25:51 +0530

    bud/cors 1

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,9 @@ app.use(cookieParser())
 
 app.use(cors({
   origin:process.env.CLIENT_URL,
-  credentials: true
-
+  credentials: true,
+ methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(express.json())
